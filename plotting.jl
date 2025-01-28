5ce6d4685300dd59c30f26cc8275803a7ac419f7
julia-sand <73934921+julia-sand@users.noreply.github.com>
2025-01-28 10:42:25 +0200

    Update plotting.jl

--- plotting.jl
+++ plotting.jl
@@ -1,32 +1,53 @@
 using Plots;
+using CSV;
+using DataFrames;
+using ImageFiltering;
+using Interpolations;
+using ForwardDiff;
 
-plot!(ylim=(0,0.6))
+##filtering functions
+function filtering(input_array,filter_delta)
+    
+    #= this function implements a simple box filter of size filter_delta and padds missing reults
+    
+    inputs: 
+    input_array - array to be smooths
+    filter_delta - size of filter, number of nearest neighbours to consider
+    
+    output:
+    filtered array with borders filled with constant value. 
+    =#
+    
+    return imfilter(vec(input_array),reflect((1/filter_delta).*centered(ones(filter_delta))),Pad(:replicate))
+    
+end;
 
-plot!(p2,legend = false)
-plot!(p2,[],[],framestyle = :none,legend = true,labels = "IPOPT", subplot = 12,fg_legend = :false)
+#get both dataframes
 
-plot!(p2,[],[],framestyle = :none,legend = true,labels = "Caluya-Halder", subplot = 12)
-plot(p2)
-savefig("overdamped_pdf.png")
+#get Caluya-Halder output
+df_calhal = CSV.read("infiniteopt/results_kl_land_new1.csv",DataFrame,header=true);
+df_ipopt = CSV.read("infiniteopt/ipopt_overdampedkl_v1.csv",DataFrame,header=true);
+
+plot_times = range(0, T, 11)
 
 
 p2 = plot(layout = 12)
 
-#evolve
-
+##density plots
 for t in 1:11
 
     curr_time = plot_times[t]
     
     
-    filtered_df = filter(row -> row.t == curr_time, df_new)
-    plot!(p2,vec(qax),(vec(uvals[t,:])),subplot=t,label = "drift output",title ="t= $curr_time",titlefontsize = 12)
+    filtered_df = filter(row -> row.t == curr_time, df_calhal)
+    filtered_df_ipopt = filter(row -> row.t == curr_time, df_ipopt)
 
 
-    fun_temp = linear_interpolation(vec(filtered_df.x),vec(filtered_df.sigma),extrapolation_bc = Line())
-    du = filtering(ForwardDiff.derivative.(Ref(fun_temp), vec(filtered_df.x)),60) #this is \partial_q V_t
+    #fun_temp = linear_interpolation(vec(filtered_df.x),
+    #du = filtering(ForwardDiff.derivative.(Ref(fun_temp), vec(filtered_df.x)),60) 
     
-    plot!(p2,vec(filtered_df.x),vec(du),subplot=t,label = "drift output")
+    plot!(p2,vec(filtered_df.x),vec(filtered_df.rho),subplot=t,label = "Caluya-Halder")
+    plot!(p2,vec(filtered_df_ipopt.x),vec(filtered_df_ipopt.rho),subplot=t,label = "InfiniteOpt",title ="t= $curr_time",titlefontsize = 12)
     
 end
 
@@ -40,37 +61,5 @@ plot!(p2,[],[],framestyle = :none,legend = true,labels = "Caluya-Halder", subplo
 
 plot(p2)
 
-savefig("overdamped_drift.png")
+savefig("overdamped_distributions.png")
 
-
-p2 = plot(layout = 12)
-
-#evolve
-
-for t in 1:11
-
-    curr_time = plot_times[t]
-    
-    
-    filtered_df = filter(row -> row.t == curr_time, df_new)
-    plot!(p2,vec(qax),(vec(vvals[t,:])),subplot=t,label = "drift output",title ="t= $curr_time",titlefontsize = 12)
-
-
-    #du = filtering(ForwardDiff.derivative.(Ref(fun_temp), vec(filtered_df.x)),60) #this is \partial_q V_t
-    
-    plot!(p2,vec(filtered_df.x),vec(filtered_df.sigma),subplot=t,label = "drift output")
-    #plot!(p2, vec(qax), 0.25*(((vec(qax))).^2 .-1).^2,subplot=t)
-    
-end
-
-
-plot!(p2,xlim =(-4,4))
-plot!(p2,legend = false)
-plot!(p2,[],[],framestyle = :none,legend = true,labels = "IPOPT", subplot = 12,fg_legend = :false)
-
-plot!(p2,[],[],framestyle = :none,legend = true,labels = "Caluya-Halder", subplot = 12)
-
-
-plot(p2)
-
-savefig("overdamped_value.png")
