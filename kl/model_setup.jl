89df64653714b468105100088b8501417db9be84
Julia Sanders <julisand@puhti-login11.bullx>
2025-01-28 10:49:34 +0200

    Changes to be committed: 	renamed:    model_setup.jl -> ep/model_setup.jl 	new file:   kl/model_setup.jl

---
+++ kl/model_setup.jl
@@ -0,0 +1,182 @@
+#get all the packies
+using InfiniteOpt, Distributions, Ipopt;
+#using Plots;
+using Trapz;
+using CSV;
+using DataFrames;
+#using Interpolations;
+using ForwardDiff;
+#using ImageFiltering;
+#using Polynomials;
+##filtering functions
+
+#get the normalisation constants
+
+
+
+
+#intial and final distributions
+###########
+global function p_initial(y)
+
+    return exp.(-((y.-1).^4)/4)
+end;
+
+
+global function p_final(y)
+
+    return exp.(-(((y.^2).-1).^2)/4);
+end;
+
+#calculate normalisation for distributions
+norm_range = Array(range(-8,8,8000))
+
+global normfinal = abs.(trapz(norm_range,p_final(norm_range)));
+global norminitial = abs.(trapz(norm_range,p_initial(norm_range)));
+
+
+#intial and final distributions
+###########
+global function underdamped_p_initial(p,q)
+
+    return exp.(-(((q.-1).^4)/4 .+ (p.^2)/2))/norminitial
+end;
+
+
+global function underdamped_p_final(p,q)
+
+    return exp.(-((((q.^2).-1).^2)/4 .+ (p.^2)/2))/normfinal;
+end;
+
+#get Caluya-Halder output
+#df_new = CSV.read("results_kl_land_new1.csv",DataFrame,header=true);
+
+#time_interval = vec(unique(df_new.t))
+#global time_interval = round.(time_interval;digits = 4)
+
+#global time_grid = Int(length(time_interval));
+
+
+#global time_steps_vec = [round.(time_interval[Int(k+1)]-time_interval[Int(k)];digits =3) for k in 1:length(time_interval)-1];
+#global plot_times = time_interval[begin:4:end]; #these times are used in the plots
+
+##LATTICE PARAMETERS!!!
+T = 1 #0.2
+num_supports_t = 11
+num_supports_q = 10
+num_supports_p = 10
+
+##################################
+#Get the model
+#create a new model object
+model = InfiniteModel(Ipopt.Optimizer);
+
+#time
+@infinite_parameter(model, t in [0, T], num_supports = num_supports_t)
+
+#space
+@infinite_parameter(model, q in [-3, 3], num_supports = num_supports_q)
+
+#space
+@infinite_parameter(model, p in [-3, 3], num_supports = num_supports_p)
+
+
+#let's define some good initial guesses
+function u_init(t,y)
+    return ((y-1)^3)/10
+end
+    
+function v_init(t,y)
+    #p = Polynomial([0.012501817,1.7759079, 
+    #        -0.98531216, 0.29115227, 
+    #        0.16276835, -0.04258577,0.01356363], :y)
+    return ((1/4)*((y^2 - 1)^2))#/10
+end
+    
+function rho_init(t,y)
+    return p_initial(y)/norminitial
+end
+
+
+#define the variables
+#first remove any existing definitions
+#unregister(model, :u)
+#unregister(model, :v)
+#unregister(model, :rho)
+
+
+#the optimal control
+@variable(model, u, Infinite(t,q), start = u_init)
+
+#the density
+@variable(model, rho>=0, Infinite(t,q), start = rho_init)
+
+#the optimal control
+@variable(model, v, Infinite(t,q), start = v_init)
+
+#define the objective
+@objective(model, Min, integral(integral((u^2)*rho,q), t)/4)
+
+#fokker planck
+@constraint(model, deriv(rho,t) - deriv(u*rho,q) - deriv(rho,q,q)== 0)
+#@constraint(model, deriv(rho,t) - 10*deriv(u*rho,q) - deriv(rho,q,q)== 0)
+
+#hjb
+#@constraint(model, deriv(v,t) - 10*u*deriv(v,q) + deriv(v,q,q) + 2.5*(u^2) == 0)
+@constraint(model, deriv(v,t) - u*deriv(v,q) + deriv(v,q,q) + (u^2)/4 == 0)
+
+#stationarity condition
+@constraint(model, 2*u == deriv(v,q))
+
+#boundary conditions on rho
+@constraint(model, rho(0,q) == exp(-((q-1)^4)/4)/norminitial)
+@constraint(model, rho(T,q) == exp(-((q^2-1)^2)/4)/normfinal)
+
+#normalisation condition
+@constraint(model, integral(rho,q) == 1)
+@constraint(model, rho(t,-3) == 0)
+@constraint(model, rho(t,3) == 0)
+
+
+# SOLVE THE MODEL
+optimize!(model)
+
+
+print(termination_status(model))
+
+tgrid = [supports(rho)[i][1] for i in 1:length(supports(rho))]
+qgrid = [supports(rho)[i][2] for i in 1:length(supports(rho))];
+
+rhovals = reshape(value(rho),(num_supports_t,num_supports_q));
+vvals = reshape(value(v),(num_supports_t,num_supports_q));
+uvals = reshape(value(u),(num_supports_t,num_supports_q));
+
+qax = vec(unique(qgrid))
+times_vec = vec(unique(tgrid))
+
+#save results to csv
+####save a csv file  
+file_name = "infiniteopt/ipopt_overdampedkl_v1.csv"
+
+# Define the header as an array of strings
+row = ["t" "x" "du" "v" "rho"]
+header = DataFrame(row,["t", "x", "du", "v", "rho"])
+
+# Write the header to a new CSV file
+CSV.write(file_name, header;header =false)
+
+
+for j in 1:num_supports_t
+     df = DataFrame([times_vec[j] .*vec(ones(num_supports_q)),
+                    vec(qax),
+                    vec(uvals[j,:]),
+                    vec(vvals[j,:]),
+                    vec(rhovals[j,:])],
+                    ["t", "x", "du", "v", "rho"])
+
+    CSV.write(file_name, df, append =true)
+    
+end
+
+
+
