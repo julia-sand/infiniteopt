740c6e4652cd422d67989edfe4a98240816fa7d4
julia-sand <73934921+julia-sand@users.noreply.github.com>
2025-01-29 10:07:28 +0200

    Update model_setup.jl

--- kl/model_setup.jl
+++ kl/model_setup.jl
@@ -5,33 +5,15 @@ using CSV;
 using DataFrames;
 using ForwardDiff;
 
-
-#####boundary conditions#######
-global function p_initial(y)
-
-    return exp.(-((y.-1).^4)/4)
-end;
+include("infiniteopt/kl/params.jl")
+#=
+Setup the infinite opt model 
+=#
 
 
-global function p_final(y)
-
-    return exp.(-(((y.^2).-1).^2)/4);
-end;
-
-#calculate normalisation for distributions
-norm_range = Array(range(-8,8,8000))
-
-global normfinal = abs.(trapz(norm_range,p_final(norm_range)));
-global norminitial = abs.(trapz(norm_range,p_initial(norm_range)));
-
-
-##LATTICE PARAMETERS!!!
-T = 0.2
-num_supports_t = 101
-num_supports_q = 1000
 
 ##################################
-#Get the model
+#Setup the model
 #create a new model object
 model = InfiniteModel(Ipopt.Optimizer);
 
