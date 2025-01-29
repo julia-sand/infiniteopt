84fa11c2951dc30d0238acf30f54e124cddb1499
julia-sand <73934921+julia-sand@users.noreply.github.com>
2025-01-29 10:17:03 +0200

    Update params.jl

--- kl/params.jl
+++ kl/params.jl
@@ -1,7 +1,49 @@
-using Trapz;
+using Trapz
+using ArgParse
+
 #=
 This file sets up boundary conditions and parameters shared by all KL models
+Arguments can be customised from the command line (parsed here)
 =#
+
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--tf"
+            help = "final time of the overdamped problem"
+            arg_type = Float
+            default = 0.2
+        "--tsteps"
+            help = "number of time coordinates in discretisation"
+            arg_type = Int
+            default = 11
+        "--qsteps"
+            help = "number of space coordinates in discretisation"
+            arg_type = Int
+            default = 20
+    end
+
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+    println("Parsed args:")
+    for (arg,val) in parsed_args
+        println("  $arg  =>  $val")
+    end
+end
+
+main()
+
+
+##LATTICE PARAMETERS!!!
+T = 0.2
+num_supports_t = 101
+num_supports_q = 1000
+
 
 
 #####boundary conditions#######
@@ -22,8 +64,3 @@ norm_range = Array(range(-8,8,8000))
 global normfinal = abs.(trapz(norm_range,p_final(norm_range)));
 global norminitial = abs.(trapz(norm_range,p_initial(norm_range)));
 
-
-##LATTICE PARAMETERS!!!
-T = 0.2
-num_supports_t = 101
-num_supports_q = 1000
